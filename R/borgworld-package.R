83534ae806c22ad4478e7127539ed715489a5c99
Steve Borgatti <steveborgatti@gmail.com>
2025-09-30 09:25:16 -0400

    updated the help procedures

---
+++ R/borgworld-package.R
@@ -0,0 +1,10 @@
+#' borgworld: Simple statistical and data processing utilities
+#'
+#' All functions are prefixed with `b`.
+#'
+#' @section Getting started:
+#' \code{help(package = "borgworld")} lists all topics.
+#' \code{browseVignettes("borgworld")} opens tutorials.
+#'
+#' @keywords internal
+"_PACKAGE"
